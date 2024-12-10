aff4aa0c70ec6141a94f036afa7d4eb469ed832f
Vanshika Gupta <vg11072001@gmail.com>
2024-12-10 11:45:20 +0530

    updated format for transformer notes

---
+++ ml notes/Theory/NN ZerotoHero Series/lectures/gpt/GPT-Transformer.md
@@ -0,0 +1,137 @@
+## Roots:
+
+- Attention Is All You Need : [PDF](nn-zero-to-hero/lectures/11.Attention-All-You-Need.pdf)
+- [github.com/karpathy/LLM101n](https://github.com/karpathy/LLM101n)
+
+---
+## 1. 3Blue1Brown 
+
+### Attention in transformers, visually explained | DL6 [Link](https://www.youtube.com/watch?v=eMlx5fFNoYc&t=269s)
+============================================================================
+
+Here are a few other relevant resources
+
+- What does it mean for computers to un...  @vcubingx 
+- [ChatGPT: 30 Year History (How AI Lear...  ](https://www.youtube.com/watch?v=OFS90-FX6pg) @ ArtOfTheProblem
+- **how directions in embedding spaces have meaning** : Efficient Estimation of Word Representations in Vector Space : https://arxiv.org/pdf/1301.3781.pdf
+
+If you're interested in the herculean task of interpreting what these large networks might actually be doing, the Transformer Circuits posts by Anthropic are great. In particular, it was only after reading one of these that I started thinking of the combination of the value and output matrices as being a combined low-rank map from the embedding space to itself, which, at least in my mind, made things much clearer than other sources.
+- A Mathematical Framework for Transformer Circuits : https://transformer-circuits.pub/2021/framework/index.html
+- https://transformer-circuits.pub/
+
+#### Links to explore:
+- A Mathematical Framework for Transformer Circuits : https://transformer-circuits.pub/2021/framework/index.html
+
+
+### How might LLMs store facts | DL7 [Link](https://www.youtube.com/watch?v=9-Jl0dxWQs8&list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi&index=8)
+============================================================================
+
+Unpacking the multilayer perceptron's in a transformer, and how they may store facts
+
+Sections:
+* 0:00 - Where facts in LLMs live
+* 2:15 - Quick refresher on transformers
+* 4:39 - Assumptions for our toy example
+* 6:07 - Inside a multilayer perceptron
+* 15:38 - Counting parameters
+* 17:04 - Superposition
+* 21:37 - Up next
+
+
+Resources: 
+- AI Alignment forum post from the Deepmind researchers referenced at the video's start:
+	- Fact Finding: Attempting to Reverse-Engineer Factual Recall on the Neuron Level [(Post 1)](https://www.alignmentforum.org/posts/iGuwZTHWb6DFY3sKB/fact-finding-attempting-to-reverse-engineer-factual-recall)
+
+- Anthropic posts about superposition referenced near the end:
+	- https://transformer-circuits.pub/2022/toy_model/index.html 
+	- https://transformer-circuits.pub/2023/monosemantic-features
+
+
+Some added resources for those interested in learning more about mechanistic interpretability, offered by Neel Nanda
+
+- Mechanistic interpretability paper reading list
+    - An Extremely Opinionated Annotated List of My Favorite Mechanistic Interpretability [Papers v2](https://www.alignmentforum.org/posts/NfFST5Mio7BCAQHPA/an-extremely-opinionated-annotated-list-of-my-favourite)
+
+- Getting started in mechanistic interpretability
+	- https://www.neelnanda.io/mechanistic-interpretability/getting-started 
+
+- An interactive demo of sparse autoencoders (made by Neuronpedia)
+	- https://www.neuronpedia.org/gemma-scope#main
+
+- Coding tutorials for mechanistic interpretability (made by ARENA)
+	- https://arena3-chapter1-transformer-interp.streamlit.app/
+
+---
+## 2. Jay Alamar Language AI & NLP Playlist [Link](https://www.youtube.com/playlist?list=PLTx9yCaDlo1UlgZiSgEjq86Zvbo2yC87d)
+
+- https://jalammar.github.io/
+- https://newsletter.languagemodels.co/ 
+
+### 2nd Transformer Video
+
+- Blog Post https://jalammar.github.io/illustrated-transformer/
+  
+- ![alt text](image-1.png)
+- ![alt text](image-2.png)
+#### Go Forth And Transform
+I hope you’ve found this a useful place to start to break the ice with the major concepts of the Transformer. If you want to go deeper, I’d suggest these next steps:
+- Read the [Attention Is All You Need](https://arxiv.org/abs/1706.03762) paper, the Transformer blog post ([Transformer: A Novel Neural Network Architecture for Language Understanding](https://ai.googleblog.com/2017/08/transformer-novel-neural-network.html)), and the [Tensor2Tensor announcement](https://ai.googleblog.com/2017/06/accelerating-deep-learning-research.html).
+- Watch [Łukasz Kaiser’s talk](https://www.youtube.com/watch?v=rBCqOTEfxvg) walking through the model and its details
+- Play with the [Jupyter Notebook provided as part of the Tensor2Tensor repo](https://colab.research.google.com/github/tensorflow/tensor2tensor/blob/master/tensor2tensor/notebooks/hello_t2t.ipynb)
+- Explore the [Tensor2Tensor repo](https://github.com/tensorflow/tensor2tensor).
+
+#### Follow-up works:
+- [Depthwise Separable Convolutions for Neural Machine Translation](https://arxiv.org/abs/1706.03059)
+- [One Model To Learn Them All](https://arxiv.org/abs/1706.05137)
+- [Discrete Autoencoders for Sequence Models](https://arxiv.org/abs/1801.09797)
+- [Generating Wikipedia by Summarizing Long Sequences](https://arxiv.org/abs/1801.10198)
+- [Image Transformer](https://arxiv.org/abs/1802.05751)
+- [Training Tips for the Transformer Model](https://arxiv.org/abs/1804.00247)
+- [Self-Attention with Relative Position Representations](https://arxiv.org/abs/1803.02155)
+- [Fast Decoding in Sequence Models using Discrete Latent Variables](https://arxiv.org/abs/1803.03382)
+- [Adafactor: Adaptive Learning Rates with Sublinear Memory Cost](https://arxiv.org/abs/1804.04235)
+
+### Others: Explainable AI Cheat Sheet - Five Key Categories [link](https://www.youtube.com/watch?v=Yg3q5x7yDeM&list=PLTx9yCaDlo1UN8ACYqzjq2tj2m2sVr3gB&index=1)
+
+![](Pasted%20image%2020241206183838.png)
+
+![](Pasted%20image%2020241206183900.png)
+
+Resources : 
+- The Explainable AI Cheat Sheet: [https://ex.pegg.io](https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqazNxcmVsTWtWY2FiaURDeGgtQzBlbXBKa1JSd3xBQ3Jtc0tuaWdiZ1IycGVBMG8tempGYzVEdHAxOFRYY0M5aGZYZ2ozT2c0T1pTUldKX0NGaTc2UE9GOGJtbUhNLXVIRXZZNzV4Y2F1Qkt5RGhKcXdrWFl5ZU94d0NkMmN2WXJKMEVUQmNCVjFhTm9VTmZOM0hWdw&q=https%3A%2F%2Fex.pegg.io%2F&v=Yg3q5x7yDeM) 
+- Mirror: [https://github.com/arpeggiohq/explain...](https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbWtROGFmUDZ3NlZzbTJOTk1WZVBncEpESG56QXxBQ3Jtc0ttcHItM3BEUThibUZiTjVjdDk2dUZobGo1b1VFenlWZllUbGV0TlU1bzRaUVV0NkNpQTFRZDV2bXpkX2VHODA1ajNteXZNUERwRkZuSnBlYWJ5b2xON182SktaSHVlLVJhQnN2TmdlMDRlajNteTlQYw&q=https%3A%2F%2Fgithub.com%2Farpeggiohq%2Fexplainabile-ai&v=Yg3q5x7yDeM) 
+- Interpretable Machine Learning Book (Christoph Molnar): [https://christophm.github.io/interpre...](https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbWg3ZXoxNU14eWFCX0VFY2JQSVJpNktMQUpIQXxBQ3Jtc0ttV1RuczZXQVRIUEwtY3ZQTTZGQ1NCNmxRaUxQVTFWZDhzVEZWTUllUWFuaXRGTzJIYTB2LWlNdjlnZGlWSHo2YnNEWGtTclB2Vm02WFVBcTQ5Tkh2dDRwdFItQzQ5eTRYaTZoZk9IcUNkV3BPQjFxYw&q=https%3A%2F%2Fchristophm.github.io%2Finterpretable-ml-book%2F&v=Yg3q5x7yDeM) 
+- Explainability for NLP (Isabelle Augenstein):    [Practical Talk 1: Explainability for ...](https://www.youtube.com/watch?v=XiaTNOHPpuA&t=0s)   
+- NLP Highlights: Interpreting NLP Model Predictions (Sameer Singh):   [117-interpreting-nlp-model-predictions-wit...](https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbE9hdWJMcmk2VEpIdnZYelNqdnNkRlRBWDAtUXxBQ3Jtc0tuVnFVTTFIdnJha2dhUkh3VG9sdGkwV092d0J0eWpSQmVoUURBMlFEOXp4Q1hUT0xsTGRobk85cjk0Nmd0cV9VazBWdTlqemtDMWVwcXhTdmxoWWtENVpESkNsb1RzMFJscXNZRUdhQVdGaDF2SElCYw&q=https%3A%2F%2Fsoundcloud.com%2Fnlp-highlights%2F117-interpreting-nlp-model-predictions-with-sameer-singh&v=Yg3q5x7yDeM)   Please Stop Doing "Explainable" ML (Cynthia Rudin):    [Please Stop Doing "Explainable" ML - ...](https://www.youtube.com/watch?v=I0yrJz8uc5Q&t=0s)
+
+
+
+## 3. Courses 
+
+### Generative AI with LLM [coursera](https://www.coursera.org/learn/generative-ai-with-llms)
+
+### Large Language Models: Apply and Implement Strategies for Large Language Models [Book]()
+
+- GitHub: https://github.com/peremartra/Large-Language-Model-Notebooks-Course/tree/main
+
+### Finetuning Large Language Models [coursera]
+
+### Stanford CS25 - Transformers United [Playlist](https://www.youtube.com/playlist?list=PLoROMvodv4rNiJRchCzutFw5ItR_Z27CM)
+- https://web.stanford.edu/class/cs25/
+- Aligning Open Language Models : ![](Pasted%20image%2020241206184535.png)
+
+### Stanford CS224N: Natural Language
+## 4. Medium/ other blogs: 
+
+### a. Amazing Things Happen When Attention Heads Are Supercharged Using Mixture-Of-Experts [Link](https://levelup.gitconnected.com/amazing-things-happen-when-attention-heads-are-supercharged-using-mixture-of-experts-b55a6b9a0ac8)
+
+#### Further Reading
+- [Research paper titled ‘MoH: Multi-Head Attention as a Mixture of Head Attention’ published in ArXiv](https://arxiv.org/abs/2410.11842v1)
+- [GitHub repository containing the code for MoH Attention](https://github.com/SkyworkAI/MoH)
+- [Research paper titled ‘Mixture of Attention Heads: Selecting Attention Heads Per Token’ published in ArXiv](https://arxiv.org/pdf/2210.05144)
+
+### b. RAG From Scratch [Link](https://pub.towardsai.net/rag-from-scratch-66c5eff02482)
+- Good explanations
+
+cccc
+### c. LLAMA 2 from gorund up [Link](https://cameronrwolfe.substack.com/p/llama-2-from-the-ground-up)
---
+++ ml notes/Theory/NN ZerotoHero Series/lectures/gpt/ng-video-lecture/gpt.py
@@ -0,0 +1,347 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+# hyperparameters
+
+max_iters = 5000 # overall epochs for training
+eval_interval = 500 # on steps of training
+
+learning_rate = 3e-4 # learning rate for the optimizer
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+eval_iters = 200 # how many iters to use for evaluation to estimate loss
+
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+
+n_embd = 384 # embedding dimension
+n_head = 6 # number of heads
+n_layer = 6 # number of layers
+
+dropout = 0.2
+# ------------
+
+torch.manual_seed(1337)
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    """
+    Estimates the average loss of the model on the training and validation datasets.
+
+    This function evaluates the model's performance on both the training and validation datasets
+    by computing the average loss over a specified number of iterations. The model is set to evaluation mode
+    during this process to disable dropout and batch normalization layers.
+
+    Parameters:
+    None
+
+    Returns:
+    dict: A dictionary containing the average loss for both the training and validation datasets.
+    The keys of the dictionary are 'train' and 'val', corresponding to the training and validation losses, respectively.
+    """
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Linear(nn.Module):
+    r"""Applies an affine linear transformation to the incoming data: :math:`y = xA^T + b`.
+
+    This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
+
+    On certain ROCm devices, when using float16 inputs this module will use :ref:`different precision<fp16_on_mi200>` for backward.
+
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        bias: If set to ``False``, the layer will not learn an additive bias.
+            Default: ``True``
+
+    Shape:
+        - Input: :math:`(*, H_{in})` where :math:`*` means any number of
+          dimensions including none and :math:`H_{in} = \text{in\_features}`.
+        - Output: :math:`(*, H_{out})` where all but the last dimension
+          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
+
+    Attributes:
+        weight: the learnable weights of the module of shape
+            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
+                If :attr:`bias` is ``True``, the values are initialized from
+                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
+                :math:`k = \frac{1}{\text{in\_features}}`
+
+    Examples::
+
+        >>> m = nn.Linear(20, 30)
+        >>> input = torch.randn(128, 20)
+        >>> output = m(input)
+        >>> print(output.size())
+        torch.Size([128, 30])
+    """
+
+
+    def __init__( self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None,) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.parameter.Parameter(torch.empty((out_features, in_features), **factory_kwargs))
+        if bias:
+            self.bias = nn.parameter.Parameter(torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter("bias", None)
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+        # https://github.com/pytorch/pytorch/issues/57109
+        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            torch.nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return F.linear(input, self.weight, self.bias)
+
+    def extra_repr(self) -> str:
+        return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
+
+
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs (headsize = n_embd//n_head)) -> (B, T, hs)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+class GPTLanguageModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        """
+        Initializes the weights of the given module.
+
+        This function applies specific initialization strategies to different types of layers
+        within the model to ensure proper training dynamics. Linear layers are initialized
+        with a normal distribution, and biases are set to zero if they exist. Embedding layers
+        are also initialized with a normal distribution.
+
+        Parameters:
+        module (nn.Module): The module whose weights are to be initialized. This can be a Linear
+                            or Embedding layer within the model.
+        """
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
+    def forward(self, idx, targets=None):
+        """
+        Performs the forward pass of the GPTLanguageModel.
+
+        This function takes a batch of input indices and optional target indices,
+        applies the necessary transformations to the input, and computes the logits
+        and loss (if target indices are provided).
+
+        Parameters:
+        idx (torch.Tensor): A tensor of shape (B, T) containing input indices.
+            B represents the batch size and 
+            T represents the sequence length.
+        targets (torch.Tensor, optional): A tensor of shape (B, T) containing target indices.
+            Defaults to None.
+
+        Returns:
+        tuple: A tuple containing the logits tensor of shape (B, T, vocab_size) and the loss scalar.
+            If targets are not provided, the loss will be None.
+        """
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C) (C is the embedding dimension)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+model = GPTLanguageModel()
+m = model.to(device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
